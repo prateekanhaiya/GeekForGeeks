1473a4447f677345445159d4ebcead9f35c4255c
prateekanhaiya <prateekshankdhar499@gmail.com>
2026-09-23 14:22:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Switch Statement/switch-statement.py
@@ -0,0 +1,36 @@
+n = int(input())
+
+def number(n):
+
+    if n == 1 :
+        return "One"
+        
+    if n == 2 :
+        return "Two"
+        
+    if n == 3 :
+        return "Three"
+        
+    if n == 4 :
+        return "Four"
+        
+    if n == 5 :
+        return "Five"
+        
+    if n == 6 :
+        return "Six"
+        
+    if n == 7 :
+        return "Seven"
+        
+    if n == 8 :
+        return "Eight"
+        
+    if n == 9 :
+        return "Nine"
+        
+    else :
+        return "Unknown"
+        
+print(number(n))
+    
